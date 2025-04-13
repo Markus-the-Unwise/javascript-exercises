2d70544e4b8d1ed44f6470177e1e11603c39860a
Markus-the-Unwise <sky6308@gmail.com>
2025-04-13 16:50:31 +0800

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,42 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray,objRemove) {
+const mainArray = inputArray;
+const secondaryArray = []
+
+let i = 0
+while (arguments[i] != undefined){
+    secondaryArray.push(arguments[i]);
+    i++
+};
+console.log("Phase 1 complete")
+for (let i of secondaryArray) {
+    while (mainArray.includes(i)==true){
+        mainArray.splice(mainArray.indexOf(i),1)
+    }
+    
+}
+return mainArray
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
+/*
+function (a,b,c,d...) {
+save first arguement as main array
+
+let i = 0
+while (argument[i] isn't undefined)
+{save argument[i] into secondary array}
+i++
+}
+
+for (i of secondaryArray) {
+
+while object is still in main array
+    {obtain index of object in main array;
+    splice at index in said array;}
+
+}
+
+*/
