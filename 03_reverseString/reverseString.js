8aace21c3cc3745dbb227cbbf5445598092d892d
Markus-the-Unwise <sky6308@gmail.com>
2025-04-13 16:11:01 +0800

    Complete 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+let splitText = text.split("");
+let output = "";
+for (i = 0; i < splitText.length;i++) {
+output += splitText[splitText.length-1-i];
+}
+return output
 };
+
+console.log(reverseString(""))
 
 // Do not edit below this line
 module.exports = reverseString;
