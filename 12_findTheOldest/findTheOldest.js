4a89ed8ee899e9ca852c5daba7e5f7ea5abc6c9e
Markus-the-Unwise <sky6308@gmail.com>
2025-05-20 16:26:56 +0800

    Complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arr) {
+    let ageArr = arr.map((obj) => {
+        if ('yearOfDeath' in obj) { endDate = obj.yearOfDeath }
+        else { endDate = new Date().getFullYear() }
+        return endDate - obj.yearOfBirth;
+    });
+    // get index of oldest age
+    let oldest = 0;
+    let oldestIndex = 0
+    for (let i = 0; i < ageArr.length; i++) {
+        if (oldest < ageArr[i]) { oldest = ageArr[i]; oldestIndex = i };
+    }
+    return arr[oldestIndex]
 };
 
 // Do not edit below this line
