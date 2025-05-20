370ef1bd7be410a48bd92e5901dc5cd9856357af
Markus-the-Unwise <sky6308@gmail.com>
2025-05-20 09:51:02 +0800

    Complete 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function (first, second) {
+  return parseFloat(first) + parseFloat(second);
 };
 
-const subtract = function() {
-	
+const subtract = function (first, second) {
+  return parseFloat(first) - parseFloat(second);
 };
 
-const sum = function() {
-	
+const sum = function (arr = []) {
+  return arr.reduce((sum, value) => sum + value, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr = []) {
+  return arr.reduce((product, value) => product * value, 1);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function (value) {
+  let a = parseInt(value);
+  let fact = 1;
+  if (a == 0) { return 1 };
+  if (a < 0) { return "ERROR" }
+  for (let i = 2; i <= a; i++) {
+    fact = fact * i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
